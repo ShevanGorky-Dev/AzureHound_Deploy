390523977101a0f81c5a8151e53d60c5024b2dae
01customuser <s.gorky@outlook.com>
2023-07-13 17:14:36 +0200

    az

---
+++ aci.ps1
@@ -0,0 +1,21 @@
+﻿$containerName = "azurehound"
+$resourceGroupName = "RGcontainer"
+$image = "azurehound.azurecr.io/latest:latest"
+$restartPolicy = "Never"
+$environmentVariables = @{
+    "AZ_STORAGE_ACCOUNT" = "acistorage123345"
+    "AZ_STORAGE_CONTAINER" = "acicontainer123345"
+}
+$cpu = 4
+$memory = 8
+$assignIdentity = "[system]"
+
+az container create `
+    --name $containerName `
+    --resource-group $resourceGroupName `
+    --image $image `
+    --restart-policy $restartPolicy `
+    --environment-variables $environmentVariables `
+    --cpu $cpu `
+    --memory $memory `
+    --assign-identity $assignIdentity
